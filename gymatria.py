15dae5930f72dd697b5667c6a61c52f75aa22002
Erel Segal-Halevi <erelsgl@gmail.com>
2026-03-26 14:38:32 +0200

    error fix

--- gymatria.py
+++ gymatria.py
@@ -24,7 +24,7 @@ class Gymatria:
     
     def __add__(self , other) -> int:
         if type(other)== int or type(other)== float:
-            return self.expr_value + int(other) + 1
+            return self.expr_value + int(other) 
         return self.expr_value + other.expr_value 
 
     def __sub__(self , other) -> int:
